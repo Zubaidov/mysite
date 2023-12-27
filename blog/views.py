62b02611964fdb194b9134c84064c8b4299d7d23
Zubaidov <zubaidov.shohruz@gmail.com>
2023-12-28 00:15:11 +0400

    almost done with comment system

--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,12 @@ def post_list(request):
     return render(request, 'blog/post/list.html', context)
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
+                             slug=post,
+                             publish__year = year,
+                             publish__month = month,
+                             publish__day = day,
                              status=Post.Status.PUBLISHED)
     context = {
         'post':post
